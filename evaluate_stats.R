483c09aad817e9c42836829edad7074621adc452
Ian Gow <iandgow@gmail.com>
2021-04-07 08:50:51 -0400

    Switched to .zip file for output.

--- evaluate_stats.R
+++ evaluate_stats.R
@@ -2,13 +2,13 @@ library(readr)
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr)
 
-# setwd("~/Downloads/prediciton_rusboost28_2003-2008")
+get_stats <- function(csv_file) {
 
-get_stats <- function(year) {
+  year <- gsub("^.*_(\\d+)\\.csv$", "\\1", csv_file)
 
-  read_csv(paste0("prediction_rusboost28_", year, ".csv"),
-             col_names = c("year", "gvkey", "y_test", "pred"),
-             col_types = "iild") %>%
+  read_csv(csv_file,
+           col_names = c("year", "gvkey", "y_test", "pred"),
+           col_types = "iild") %>%
     mutate(y_pred = pred >= quantile(pred, probs=0.99)) %>%
     count(y_test, y_pred) %>%
     pivot_wider(names_from = c(y_test, y_pred), values_from = n) %>%
@@ -16,8 +16,14 @@ get_stats <- function(year) {
     select(year, everything())
 }
 
+get_file_list <- function() {
+  temp_path <- tempdir()
+  unzip("prediction_rusboost28_1999-2014.zip", exdir = temp_path)
+  list.files(temp_path, "*.csv", full.names = TRUE)
+}
+
 results <-
-  bind_rows(lapply(2003:2009L, get_stats)) %>%
+  bind_rows(lapply(get_file_list(), get_stats)) %>%
   rename(tp = `TRUE_TRUE`,
          tn = `FALSE_FALSE`,
          fp = `FALSE_TRUE`,
@@ -25,4 +31,10 @@ results <-
   mutate(tp = coalesce(tp, 0))
 
 results %>%
+  mutate(ppv = tp/(tp+fp)) %>%
+  print(n = Inf)
+
+
+results %>%
+  summarize(across(tn:tp, sum)) %>%
   mutate(ppv = tp/(tp+fp))
