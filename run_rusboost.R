6b8bf286d9a2a66701429c731a0e375d38b118dc
Ian Gow <iandgow@gmail.com>
2022-09-24 20:24:11 +1000

    Made model smaller for development speed.

--- run_rusboost.R
+++ run_rusboost.R
@@ -43,7 +43,7 @@ formula <- paste(y_var, "~", paste(X_vars, collapse = " + "))
 
 
 set.seed(2021)
-fm <- rus(formula, data_train, size = 300)
+fm <- rus(formula, data_train, size = 30)
 scores <- predict(fm, data_test, type = "prob")
 fit <- predict(fm, data_test, type = "class")
 auc(as.numeric(data_test$misstate), as.numeric(scores))
